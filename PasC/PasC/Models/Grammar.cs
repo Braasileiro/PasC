117ce79eda527dded44726054ef5f18e3eba3411
Lucas Cota <lucasbrunocota@live.com>
2018-03-19 16:40:15 -0300

    Expressões regulares e tabela de símbolos implementada.
    Ambas expressões regulares foram devidamente testadas.

---
+++ PasC/PasC/Models/Grammar.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PasC.Models
+{
+	class Grammar
+	{
+		// Regex
+		public static Regex DIGIT     = new Regex("[0-9]");
+		public static Regex LETTER    = new Regex("[a-zA-Z]");
+		public static Regex NUMCONST  = new Regex("[0-9]+(.[0-9]+)?");
+		public static Regex CHARCONST = new Regex("['][\x00-\x7F][']");
+		public static Regex LITERAL   = new Regex("[\"][\x00-\x7F]*[\"]");
+		public static Regex ID        = new Regex("[a-zA-Z]+[a-zA-Z 0-9]*");
+
+		// Symbol Table
+		public static Dictionary<string, string> SYMBOLS = new Dictionary<string, string>()
+		{
+			// Keyword
+			{ "program", "KW" },
+			{ "if", "KW" },
+			{ "else", "KW" },
+			{ "while", "KW" },
+			{ "read", "KW" },
+			{ "write", "KW" },
+			{ "num", "KW" },
+			{ "char", "KW" },
+			{ "and", "KW" },
+			{ "or", "KW" },
+			{ "not", "KW" },
+
+			// Operator
+			{ ">", "OP_GT" },
+			{ "<", "OP_LT" },
+			{ "+", "OP_AD" },
+			{ "-", "OP_MIN" },
+			{ "*", "OP_MUL" },
+			{ "/", "OP_DIV" },
+			{ "=", "OP_ASS" },
+			{ "==", "OP_EQ" },
+			{ "!=", "OP_NE" },
+			{ ">=", "OP_GE" },
+			{ "<=", "OP_LE" },
+
+			// Symbol
+			{ "{", "SMB_OBC" },
+			{ "}", "SMB_CBC" },
+			{ "(", "SMB_OPA" },
+			{ ")", "SMB_CPA" },
+			{ ",", "SMB_COM" },
+			{ ";", "SMB_SEM" },
+
+			/*
+			 * Dynamic Runtime Symbols
+			 * ID: ID
+			 * LIT: LITERAL
+			 * CON_NUM: NUMCONST
+			 * CON_CHAR: CHARCONST
+			 */
+
+			// ...
+		};
+	}
+}
