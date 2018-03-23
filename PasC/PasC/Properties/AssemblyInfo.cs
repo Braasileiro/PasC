7946612da7b2e9f90865e5075e6047ca7b8a5491
Lucas Cota <lucasbrunocota@live.com>
2018-03-23 14:42:58 -0300

    Versionando os atributos do objeto de saída.

--- PasC/PasC/Properties/AssemblyInfo.cs
+++ PasC/PasC/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PasC")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Lucas Cota, Carlos Alberto © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2018.1.1")]
+[assembly: AssemblyFileVersion("2018.1.1")]
